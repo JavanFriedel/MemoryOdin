ae7820f165222c894c93ffbb9fed5f4292b58292
JavanFriedel <javanfriedel@hotmail.com>
2024-05-24 15:26:58 -0400

    Initial commit with tailwind installed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  const [gameMode, setGameMode] = useState("")
+ 
+  return <div className='underline bg-red-400'>Hello World1</div>
+}
+
+export default App
